2e341ad1455478c5d82a2557e8b9ba25b0f319bf
Md. Jueal <mdjueal920977@gmail.com>
2024-12-29 18:23:07 +0600

    fix: init repo

--- src/layouts/AppLayout/index.js
+++ src/layouts/AppLayout/index.js
@@ -1,13 +1,9 @@
-import Footer from "@/widget/Footer";
-import Header from "@/widget/Header";
 import React from "react";
 
 const AppLayout = ({ children }) => {
     return (
         <div className=" relative">
-            {/* <Header /> */}
             <div>{children}</div>
-            <Footer />
         </div>
     );
 };
